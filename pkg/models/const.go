5471281f8b047f9998a7bc708493dee882cb78cc
webtimr <swishtim@gmail.com>
2023-10-30 11:44:51 +0400

    description ++

--- pkg/models/const.go
+++ pkg/models/const.go
@@ -2,9 +2,9 @@ package models
 
 const (
 	StartCommandString         = "what's up? my name is Rosetta. i could help you with anything, but only when i feel like it."
-	SettingCommandString       = ""
-	AccountCommandString       = ""
-	GenerateImageCommandString = ""
-	HelpCommandString          = ""
-	UnknownCommandString       = ""
+	SettingCommandString       = "Here you can see a description of each GPT model"
+	AccountCommandString       = "This is your profile, here you will see the status of your subscription"
+	GenerateImageCommandString = "Here you can generate images, this requires a subscription"
+	HelpCommandString          = "Here you see a list of commands to make using the bot easier"
+	UnknownCommandString       = "You entered an unknown command"
 )
